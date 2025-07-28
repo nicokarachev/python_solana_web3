ce3bfacbbe73909c534b06a0e8ac0dbc65dd00a0
nicokarachev <nicokarachev.dev@gmail.com>
2025-07-28 05:57:06 +0000

    update

--- main.py
+++ main.py
@@ -11,3 +11,4 @@ mint_address = PublicKey("Es9vMFrzaCERU8D33Gdn5i2et2jTFoLQxkaFWwoTxkD9")
 # Get token supply
 response = client.get_token_supply(mint_address)
 print("Total supply:", response["result"]["value"]["uiAmountString"])
+
