d8d1145914e39a33277248a6bdadfae008713739
nicokarachev <nicokarachev.dev@gmail.com>
2025-07-28 05:47:18 +0000

    update

--- main.py
+++ main.py
@@ -11,5 +11,3 @@ mint_address = PublicKey("Es9vMFrzaCERU8D33Gdn5i2et2jTFoLQxkaFWwoTxkD9")
 # Get token supply
 response = client.get_token_supply(mint_address)
 print("Total supply:", response["result"]["value"]["uiAmountString"])
-
-https://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@github.com/nicokarachev/python_solana_web3.git
